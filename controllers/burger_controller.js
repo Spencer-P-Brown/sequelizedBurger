3fa2d3fc02a2d848d9b1e499f937276da8742b21
Spencer-P-Brown <Spencer-P-Brown@github.com>
2017-08-04 17:48:06 -0500

    converted from sql to sequelize, updates to all files

---
+++ controllers/burger_controller.js
@@ -0,0 +1,42 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (burger.js) to use its database functions.
+var db = require("../models");
+
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+  db.Burger.findAll(req.body).then(function(results){
+    var hbsObject = {
+        burgers: results
+    };
+      res.render("index", hbsObject);
+  });
+
+});
+
+router.post("/", function(req, res) {
+    db.Burger.create(
+        req.body
+    ).then(function(results) {
+        res.redirect("/");
+    });
+});
+
+
+router.put("/:id", function(req, res) {
+    db.Burger.update(
+        req.body,
+        {where: {
+            id: req.params.id
+        }
+    }).then(function(reslults){
+        res.redirect("/");
+    });
+});
+
+
+// Export routes for server.js to use.
+module.exports = router;
---
+++ models/burger.js
@@ -0,0 +1,17 @@
+// Export the database function for the controlelr (burger_controller.js);
+module.exports = function(sequelize, DataTypes) {
+    var Burger = sequelize.define("Burger", {
+        burger_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        devoured: {
+            type:DataTypes.BOOLEAN,
+            defaultValue: false
+        }
+    });
+    return Burger
+};
