b71e955bb5b3350701e1b13838832bca9006b2d9
Spencer-P-Brown <Spencer-P-Brown@github.com>
2017-09-04 20:08:49 -0500

    updated index.handlebars, updated config.json

--- models/burger.js
+++ models/burger.js
@@ -12,6 +12,11 @@ module.exports = function(sequelize, DataTypes) {
             type:DataTypes.BOOLEAN,
             defaultValue: false
         }
+    },
+    {
+        freezeTableName: true,
+
+        tableName: "Burger"
     });
     return Burger
 };
